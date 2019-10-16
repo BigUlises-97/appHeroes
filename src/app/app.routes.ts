1b80e2f986733dabbd63b42696035ebbcb325a0e
Fabricio <bigulises@hotmail.com>
2019-10-15 18:33:01 -0600

    app terminada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,8 +10,8 @@ const APP_ROUTES: Routes = [
     { path: 'about', component: AboutComponent },
     { path: 'heroes', component: HeroesComponent },
     { path: 'heroe/:id', component: HeroeComponent },
-    { path: 'search', component: SearchComponent },
-    { path: '**', pathMatch: 'full', redirectTo:'' }
+    { path: 'search/:termino', component: SearchComponent },
+    { path: '**', pathMatch: 'full', redirectTo:'home' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router'; //este es para los params
+import { HeroesService } from '../../services/heroes.service';
+
 
 @Component({
   selector: 'app-search',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  @Input() heroes:any[] = [];
+  termino:string;
+  idx:number;
+
+  allHeroes:any[] = [];
+
+  constructor( private activatedRoute:ActivatedRoute, private _heroesService:HeroesService, private router:Router ) {  }
 
   ngOnInit() {
+ 
+    this.activatedRoute.params.subscribe(params => {
+      this.termino = params['termino'];
+      this.heroes = this._heroesService.buscarHeroes(params['termino']);
+      console.log(this.heroes);
+    });
   }
+  /* 
+  verHeroeNombre( nombre:string ){
+    this.allHeroes = this._heroesService.getHeroes();
+    
+    for(let i=0;i<this.allHeroes.length;i++){
+      if(this.allHeroes[i].nombre == nombre){
+        this.idx = i;
+      }
+    }
+    this.router.navigate(['/heroe',this.idx]);
+  }
+*/
 
 }
